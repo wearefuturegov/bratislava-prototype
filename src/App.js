15d04c0305cb00ade08c10176a478a96b77ef3e6
Hannah <xhan104@gmail.com>
2021-02-02 12:29:32 +0000

    translation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import HeaderComponent from './HeaderComponent';
+
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from "i18next"
+import { I18nextProvider } from  "react-i18next";
+
+
+import common_sk from "./translations/sk/common.json";
+import common_en from "./translations/en/common.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+  lng: 'en',                              // language to use
+  resources: {
+      en: {
+          common: common_en               // 'common' is our custom namespace
+      },
+      sk: {
+          common: common_sk
+      },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+        <App/>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
